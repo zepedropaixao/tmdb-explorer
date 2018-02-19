c9a44d4470e40b01ce7e83ed0fa3cd8cc7f552a2
Pedro Paixão <zepedropaixao@gmail.com>
2018-02-19 00:10:25 +0000

    Added all attributes to the MovieDetail View Model class

--- app/src/main/java/me/paixao/tmdbexplorer/ui/moviedetail/MovieDetailActivity.kt
+++ app/src/main/java/me/paixao/tmdbexplorer/ui/moviedetail/MovieDetailActivity.kt
@@ -3,6 +3,8 @@ package me.paixao.tmdbexplorer.ui.moviedetail
 import android.arch.lifecycle.Observer
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_movie_detail.*
 import me.paixao.tmdbexplorer.R
 import me.paixao.tmdbexplorer.data.Movie
 import me.paixao.tmdbexplorer.databinding.ActivityMovieDetailBinding
@@ -21,39 +23,6 @@ class MovieDetailActivity : BaseActivity() {
         title = "moviExplorer"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         initViewModel()
-
-
-        /*disposables.add(repository.getMovieWithId(intent.getLongExtra("movie_id", 1))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({ result ->
-                    GlideApp.with(this)
-                            .load("https://image.tmdb.org/t/p/w500${result.backdrop_path}")
-                            .thumbnail(Glide.with(this).load(R.drawable.loader))
-                            // TODO add image for error
-                            .centerCrop()
-                            .into(backdrop)
-                    movie_title.text = result.title
-                    title = result.title
-                    movie_overview.text = result.overview
-                    movie_vote_nr.text = "${result.vote_average} vote average (${result.vote_count} total votes)"
-                    movie_rating.progress = (result.vote_average * 10).toInt()
-                    movie_rating.visibility = View.VISIBLE
-                    movie_duration.text = "${result.runtime} min"
-                    val revenue: Double = result.revenue.toDouble() / 1000000
-                    val budget: Double = result.budget.toDouble() / 1000000
-                    val budgetString = if (result.budget != 0) "(Budget: \$${budget.format(2)}M)" else ""
-                    movie_revenue.text = "Revenue: \$${revenue.format(2)}M $budgetString"
-                    if (result.budget != 0)
-                        if (result.revenue > result.budget)
-                            movie_revenue.setTextColor(resources.getColor(R.color.colorGreen))
-                        else movie_revenue.setTextColor(resources.getColor(R.color.colorPrimary))
-                    repository.isLoadingData = false
-                }, { error ->
-                    error.printStackTrace()
-                    Log.e("Error", error.message)
-                    repository.isLoadingData = false
-                }))*/
     }
 
     private fun initViewModel() {
@@ -62,6 +31,7 @@ class MovieDetailActivity : BaseActivity() {
         viewModel.getMovieWithId(intent.getLongExtra("movie_id", 1)).observe(this@MovieDetailActivity, Observer<Movie?> { movie ->
             // Bind movie to view binder
             title = movie?.title
+            movie_rating.visibility = View.VISIBLE
         })
     }
 
--- app/src/main/java/me/paixao/tmdbexplorer/ui/moviedetail/MovieDetailViewModel.kt
+++ app/src/main/java/me/paixao/tmdbexplorer/ui/moviedetail/MovieDetailViewModel.kt
@@ -3,11 +3,19 @@ package me.paixao.tmdbexplorer.ui.moviedetail
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Transformations
 import android.databinding.BaseObservable
 import android.databinding.Bindable
+import android.databinding.BindingAdapter
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import me.paixao.tmdbexplorer.R
 import me.paixao.tmdbexplorer.data.Movie
 import me.paixao.tmdbexplorer.data.source.MoviesRepository
+import me.paixao.tmdbexplorer.utils.GlideApp
 import me.paixao.tmdbexplorer.utils.SingleLiveEvent
+import me.paixao.tmdbexplorer.utils.format
 
 
 /**
@@ -23,11 +31,43 @@ class MovieDetailViewModel(
         private val moviesRepository: MoviesRepository
 ) : AndroidViewModel(context) {
 
-    val movie: LiveData<Movie?> = moviesRepository.myMovie
+    val movie: LiveData<Movie> = moviesRepository.myMovie
+
+    val movieVoteAverage: LiveData<String> = Transformations.map(
+            movie, { data ->
+        "${data.vote_average} ${context.getString(R.string.vote_average)} (${data.vote_count} ${context.getString(R.string.total_votes)})"
+    })
+
+    val movieRating: LiveData<Int> = Transformations.map(
+            movie, { data ->
+        (data.vote_average * 10).toInt()
+    })
+
+    val movieDuration: LiveData<String> = Transformations.map(
+            movie, { data ->
+        "${data.runtime} ${context.getString(R.string.min)}"
+    })
+
+    val movieBudget: LiveData<String> = Transformations.map(
+            movie, { data ->
+        val revenue: Double = data.revenue.toDouble() / 1000000
+        val budget: Double = data.budget.toDouble() / 1000000
+        val budgetString = if (data.budget != 0) "(${context.getString(R.string.budget)}: \$${budget.format(2)}M)" else ""
+        "${context.getString(R.string.revenue)}: \$${revenue.format(2)}M $budgetString"
+    })
+
+    val movieRevenueColor: LiveData<Int> = Transformations.map(
+            movie, { data ->
+        if (data.budget != 0)
+            if (data.revenue > data.budget)
+                context.resources.getColor(R.color.colorGreen)
+            else context.resources.getColor(R.color.colorPrimary)
+        else context.resources.getColor(android.R.color.white)
+    })
 
     val snackbarMessage = SingleLiveEvent<Int>()
 
-    fun getMovieWithId(movieId: Long): LiveData<Movie?> {
+    fun getMovieWithId(movieId: Long): LiveData<Movie> {
         moviesRepository.getMovie(movieId)
         return movie
     }
@@ -39,4 +79,19 @@ class MovieDetailViewModel(
     fun isLoadingData(): Boolean {
         return moviesRepository.isLoadingData
     }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("app:imagesrc")
+        fun setImageSource(v: View, s: String?) {
+            if (s != null)
+                GlideApp.with(v.context)
+                        .load("https://image.tmdb.org/t/p/w500$s")
+                        .thumbnail(Glide.with(v.context).load(R.drawable.loader))
+                        // TODO add image for error
+                        .centerCrop()
+                        .into(v as ImageView)
+        }
+    }
+
 }
